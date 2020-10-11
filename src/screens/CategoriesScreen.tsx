d4c36a839e2ed27b84a185f8ae44fdb8c57490d5
chuk-chuk <yulia.v.litvin@gmail.com>
2020-10-11 23:00:15 +0100

    Create menus grid

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,7 +1,17 @@
-import React from "react"
-import { View, Text, StyleSheet, Button } from "react-native"
+import React, { ReactElement } from "react"
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native"
 import { useNavigation } from "react-navigation-hooks"
+
 import { RouteName } from "../@types"
+import { CATEGORIES } from "../data/dummy-data"
+import Colors from "../theme/colors"
 
 const styles = StyleSheet.create({
   screen: {
@@ -9,18 +19,54 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
 })
 
 export default function CategoriesScreen() {
   const { navigate } = useNavigation()
 
+  const renderGridItem = (itemData: any): ReactElement => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() =>
+          navigate({
+            routeName: RouteName.CategoryMeals,
+            params: {
+              categoryId: itemData.item.id,
+            },
+          })
+        }
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   return (
-    <View style={styles.screen}>
-      <Text>Categories Screen</Text>
-      <Button
-        title="Go to Meals"
-        onPress={() => navigate(RouteName.CategoryMeals)}
-      />
-    </View>
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+      keyExtractor={(item): string => item.id}
+    />
   )
+}
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Categories",
+  headerTintColor: "white",
+  headerStyle: {
+    backgroundColor:
+      Platform.OS === "android" ? Colors.accentColor : Colors.primaryColor,
+    borderRadius: 15,
+    shadowColor: "green",
+    height: 100,
+  },
 }
