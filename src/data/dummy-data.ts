aba92e17c63bfacedf17aa12a6af213d8da09af3
chuk-chuk <yulia.v.litvin@gmail.com>
2020-10-11 22:58:58 +0100

    Add category data model

---
+++ src/data/dummy-data.ts
@@ -0,0 +1,14 @@
+import Category from "../models/category"
+
+export const CATEGORIES = [
+  new Category("c1", "Italian", "#f5428d"),
+  new Category("c2", "Indian", "#f5428d"),
+  new Category("c3", "Russian", "#f5428d"),
+  new Category("c4", "French", "#f5428d"),
+  new Category("c5", "British", "#f5428d"),
+  new Category("c6", "Thai", "#f5428d"),
+  new Category("c7", "Vegan", "#f5428d"),
+  new Category("c8", "Winter", "#f5428d"),
+  new Category("c9", "Spanish", "#f5428d"),
+  new Category("c10", "Summer", "#f5428d"),
+]
