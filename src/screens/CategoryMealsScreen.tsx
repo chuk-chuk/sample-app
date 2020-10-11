4a1bf3ae9b16a0f889ce0daf16b54817c1df3cb9
chuk-chuk <yulia.v.litvin@gmail.com>
2020-10-11 23:00:45 +0100

    Customise the header title using nav params

--- src/screens/CategoryMealsScreen.tsx
+++ src/screens/CategoryMealsScreen.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { View, Text, StyleSheet, Button } from "react-native"
-import { useNavigation } from "react-navigation-hooks"
+import { useNavigation, useNavigationParam } from "react-navigation-hooks"
+
 import { RouteName } from "../@types"
+import { CATEGORIES } from "../data/dummy-data"
 
 const styles = StyleSheet.create({
   screen: {
@@ -13,14 +15,31 @@ const styles = StyleSheet.create({
 
 export default function CategoryMealsScreen() {
   const { navigate } = useNavigation()
+  const categoryId = useNavigationParam("categoryId")
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === categoryId)
 
   return (
     <View style={styles.screen}>
       <Text>Category Meals Screen</Text>
+      <Text>{selectedCategory?.title}</Text>
       <Button
         title="See Details"
         onPress={() => navigate(RouteName.MealDetail)}
       />
     </View>
   )
+}
+
+CategoryMealsScreen.navigationOptions = ({ navigation }: any) => {
+  const selectedCategory = CATEGORIES.find(
+    cat => cat.id === navigation.getParam("categoryId")
+  )?.title
+
+  return {
+    headerTitle: selectedCategory,
+    headerTitleStyle: {
+      fontWeight: "bold",
+    },
+  }
 }
